03421c241e27b418dcd09c16512b421179f7c6e8
Tomas-Adrian Boboi <boboitomas@gmail.com>
2021-03-20 19:06:23 +0200

    Added rule and inference machine classes; Connection between UI and inference mechine next;

---
+++ SE-Garage/SE-Garage/Classes/InferenceMachine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Garage.Classes
+{
+    class InferenceMachine
+    {
+        public Boolean checkRuleMatch(Regula lhs, Regula rhs)
+        {
+            Boolean match = true;
+
+            if (lhs.ruleFields.Length != rhs.ruleFields.Length)
+            {
+                match = false;
+            }
+            else
+            {
+                for (int index = (int)(RuleFields.RULE_FIRST_RULE + 1); index < (int)(RuleFields.RULE_LAST_RULE) && match; index++)
+                {
+                    if (lhs.ruleFields[index] != rhs.ruleFields[index])
+                    {
+                        match = false;
+                    }
+                }
+            }
+
+            return match;
+        }
+
+        public int checkMatchAndGetOutput(Regula lhs, Regula rhs)
+        {
+            int output = -1;
+
+            if(checkRuleMatch(lhs, rhs))
+            {
+                int lhsOutput = lhs.generateOutput();
+                int rhsOutput = rhs.generateOutput();
+
+                if (lhsOutput != rhsOutput)
+                {
+                    if (lhsOutput == -1 && rhsOutput != -1)
+                    {
+                        output = rhsOutput;
+                    }
+                    else if (lhsOutput != -1 && rhsOutput == -1)
+                    {
+                        output = lhsOutput;
+                    }
+                }
+                else
+                {
+                    output = lhsOutput;
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ SE-Garage/SE-Garage/Classes/Regula.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Garage.Classes
+{
+    public enum RuleFields : int
+    {
+        RULE_FIRST_RULE = -1,
+
+        RULE_APPL_GAMING,
+        RULE_APPL_DESIGN,
+        RULE_APPL_OFFICE,
+
+        RULE_GAMING_GTAV,
+        RULE_GAMING_MINECRAFT,
+        RULE_GAMING_FORTNITE,
+        RULE_GAMING_LEAGUEOFLEGENDS,
+        RULE_GAMING_CSGO,
+        RULE_GAMING_PUBG,
+        RULE_GAMING_APEXLEGENDS,
+        RULE_GAMING_OVERWATCH,
+        RULE_GAMING_ROCKETLEAGUE,
+        RULE_GAMING_CYBERPUNK2077,
+        RULE_GAMING_CODMW,
+        RULE_GAMING_WITCHER3,
+        RULE_GAMING_TERRARIA,
+        RULE_GAMING_BORDERLANDS3,
+        RULE_GAMING_STARWARS,
+
+        RULE_GAMING_RUN_DECENT,
+        RULE_GAMING_RUN_BINE,
+        RULE_GAMING_RUN_FBINE,
+
+        RULE_DESIGN_PHOTOSHOP,
+        RULE_DESIGN_ILLUSTRATOR,
+        RULE_DESIGN_CORELIDRAW,
+        RULE_DESIGN_GIMP,
+        RULE_DESIGN_BLENDER,
+        RULE_DESIGN_3DSMAX,
+        RULE_DESIGN_PAINTSHOP,
+        RULE_DESIGN_MAYA,
+        
+        RULE_OFFICE_STOCARE,
+        RULE_OFFICE_VITEZA,
+        RULE_OFFICE_AMBELE,
+
+        RULE_LAST_RULE
+    }
+
+    class Regula
+    {
+        public Boolean[] ruleFields;
+
+        public int ruleOutput;
+
+        public Regula(int output)
+        {
+            ruleFields = new Boolean[(int)RuleFields.RULE_LAST_RULE];
+
+            for(int index = (int)(RuleFields.RULE_FIRST_RULE + 1); index < (int)RuleFields.RULE_LAST_RULE; index++)
+            {
+                ruleFields[index] = false;
+            }
+
+            ruleOutput = output;
+        }
+
+        public Regula(Boolean[] fields)
+        {
+            ruleFields = new Boolean[(int)RuleFields.RULE_LAST_RULE];
+
+            for (int index = (int)(RuleFields.RULE_FIRST_RULE + 1); index < (int)RuleFields.RULE_LAST_RULE; index++)
+            {
+                ruleFields[index] = fields[index];
+            }
+
+            ruleOutput = -1;
+        }
+
+        public Regula(Boolean[] fields, int output)
+        {
+            ruleFields = new Boolean[(int)RuleFields.RULE_LAST_RULE];
+
+            for (int index = (int)(RuleFields.RULE_FIRST_RULE + 1); index < (int)RuleFields.RULE_LAST_RULE; index++)
+            {
+                ruleFields[index] = fields[index];
+            }
+
+            ruleOutput = output;
+        }
+
+        public void activateField(RuleFields field)
+        {
+            ruleFields[(int)field] = true;
+        }
+
+        public void deactivateField(RuleFields field)
+        {
+            ruleFields[(int)field] = false;
+        }
+
+        public int generateOutput()
+        {
+            return ruleOutput;
+        }
+    }
+}
